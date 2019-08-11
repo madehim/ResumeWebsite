235f76e4243eff90d1ecdeb0a1111438c89c18f9
madehim <madehim333@yandex.ru>
2019-08-11 17:04:12 +0400

    add old-school fonts, minify js/css, sass

---
+++ ResumeWebSite/gulpfile.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+var gulp = require("gulp"),
+    rimraf = require("rimraf"),
+    sass = require("gulp-sass"); // добавляем модуль sass
+
+var paths = {
+    webroot: "./wwwroot/"
+};
+// регистрируем задачу для конвертации файла scss в css
+gulp.task("sass", function () {
+    return gulp.src(paths.webroot + '/css/site.scss')
+        .pipe(sass())
+        .pipe(gulp.dest(paths.webroot + '/css'))
+});
